01edc11affea323d0aef90527e1e6c0dd2ace0fa
Krish Khatiwada <krishkhatiwada125@gmail.com>
2024-08-11 19:23:22 +0200

    practice set destroyed

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-document.getElementsByTagName("ul")[0].firstElementChild.style.color = "red"
+// document.getElementsByTagName("ul")[0].firstElementChild.style.color = "red";
+// -------------------------------------------------------------------------------------------------------------->
+
+// document.getElementsByTagName("ul")[0].firstElementChild.style.color = "green";
+// document.getElementsByTagName("ul")[0].lastElementChild.style.color = "green";
+// -------------------------------------------------------------------------------------------------------------->
+
+Array.from(document.getElementsByTagName("li")).forEach((element) =>{
+    element.style.background = "cyan";
+})
